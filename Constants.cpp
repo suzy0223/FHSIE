aef42d2c33251c1c36be646f7e974b5ad91fdffb
suzy0223 <811192022@qq.com>
2023-10-25 12:44:55 +1100

    Add files via upload

---
+++ Constants.cpp
@@ -0,0 +1,32 @@
+#include "Constants.h"
+
+const string Constants::DATASETS = "skewed_2m";
+const string Constants::DISTRIBUTION = "skewed";
+const string Constants::INSERT_DISTRIBUTION = "normal";
+
+const string Constants::MODEL_PATH = "model/";
+const string Constants::DATA_PATH = Constants::DATASETS + "/data/";
+const string Constants::MODEL_R_PATH = "model/record/"+Constants::DATASETS+"/";
+
+const string Constants::INDEX_DIR = "dataset/" + Constants::DATASETS +  "/";
+const string Constants::INDEX_PATH = Constants::INDEX_DIR + "index_points"+to_string(Constants::LEVEL-1)+".csv";
+
+const string Constants::WORKLOAD_PATH = "workload/";
+const string Constants::WINDOW_WORKLOAD_PATH = Constants::WORKLOAD_PATH + "window/";
+const string Constants::KNN_WORKLOAD_PATH = Constants::WORKLOAD_PATH + "knn/";
+const string Constants::INSERT_WORKLOAD_PATH = Constants::WORKLOAD_PATH + "insert/";
+
+
+const string Constants::K_MEANS_PATH = "model/kmeans/" + Constants::DATASETS + "/";
+const string Constants::BLOCK_INFO = Constants::MODEL_R_PATH + "block_count.csv";
+const string Constants::CLUSTER_BLOCK = "cluster_block.csv";
+const string Constants::SPLIT_PTS = "split_pts.csv";
+const string Constants::CLUSTER_INFO = "cluster_info.csv";
+
+const string Constants::GRID_CLUSTER = Constants::MODEL_R_PATH + "grid_cluster.csv";
+const string Constants::GRID_INFO = Constants::MODEL_R_PATH + "grid_info.csv";
+const string Constants::PART_INFO = Constants::MODEL_R_PATH + "part_info.csv";
+
+Constants::Constants()
+{
+}
---
+++ Constants.h
@@ -0,0 +1,44 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+#include <string>
+using namespace std;
+class Constants
+{
+public:
+    static const int DIM = 2;
+    static const int B = 100;
+    static const int LEVEL = 3;
+    static const int SCALE = 10000;
+    static const int SKEWNESS = 4;
+    static const int DATASIZE = 2000000;
+
+    static const string DATASETS;
+    static const string DISTRIBUTION;
+    static const string INSERT_DISTRIBUTION;
+
+    static const string MODEL_PATH;
+    static const string DATA_PATH;
+    static const string MODEL_R_PATH;
+
+    static const string WORKLOAD_PATH;
+    static const string WINDOW_WORKLOAD_PATH;
+    static const string KNN_WORKLOAD_PATH;
+    static const string INSERT_WORKLOAD_PATH;
+
+    static const string INDEX_DIR;
+    static const string INDEX_PATH;
+
+    static const string K_MEANS_PATH;
+    static const string BLOCK_INFO;
+    static const string CLUSTER_BLOCK;
+    static const string SPLIT_PTS;
+    static const string CLUSTER_INFO;
+
+    static const string GRID_CLUSTER;
+    static const string GRID_INFO;
+    static const string PART_INFO;
+
+    Constants();
+};
+
+#endif
